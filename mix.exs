6d45f94d519c1f1030499c1096f5709c0a2f547b
Nick Smith <nsmith458914@gmail.com>
2020-07-27 16:31:10 -0400

    Add junit reporting.

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule DndMatchmaker.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.1"},
-      {:cowboy, "< 2.8.0", override: true}
+      {:cowboy, "< 2.8.0", override: true},
+      {:junit_formatter, "~> 3.1", only: [:test]}
     ]
   end
 
